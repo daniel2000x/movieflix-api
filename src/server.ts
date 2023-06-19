702e49dc1d83a16d504cce1b020f2b2a821fa779
Daniel Rosa <danielrosa_1@hotmail.com>
2023-06-19 10:44:05 -0300

    adcinionada a rota de listagem de filmes

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,14 @@
 import express from "express";
+import {PrismaClient} from "@prisma/client";
 
 const port = 3000;
 const app = express();
+const prisma = new PrismaClient();
 
 //GET , POST , PUT , PATCH, DELETE
-app.get("/movies", (req, res ) =>{
-    res.send("listagem de filmes");
+app.get("/movies", async (req, res ) =>{
+    const movies = await prisma.movie.findMany();
+    res.json(movies);
 });
 
 app.listen(port, () => {
