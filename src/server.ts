3015d18c859e70eed5c055fff7e5734d1ac47eca
Daniel Rosa <danielrosa_1@hotmail.com>
2023-06-20 13:28:31 -0300

    adcionada mensagem de erro de criação

--- src/server.ts
+++ src/server.ts
@@ -6,9 +6,38 @@ const app = express();
 const prisma = new PrismaClient();
 
 //GET , POST , PUT , PATCH, DELETE
-app.get("/movies", async (req, res ) =>{
-    const movies = await prisma.movie.findMany();
+app.get("/movies", async (_, res ) => {
+    const movies = await prisma.movie.findMany({
+        orderBy: {
+            title: "asc",
+        },
+        include: {
+            genres: true,
+            languages: true
+        }
+    });
     res.json(movies);
+});
+
+app.post("/movies", async (req, res) => {
+
+    const { title, genre_id, language_id, oscar_count, release_date} = req.body;
+
+    try{
+    await prisma.movie.create({
+        data: {
+            title,
+            genre_id,
+            language_id,
+            oscar_count,
+            release_date: new Date(release_date)
+        }
+    });
+    }catch(error){
+        return res.status(500).send({message:"Falha ao cadastrar um filme"});
+    }
+    
+    res.status(201) .send();
 });
 
 app.listen(port, () => {
